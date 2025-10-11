ca2dd6e1c503c2200ef1299cb1e94b5c9b616fd7
basharatali-web <softwaredivision92@gmail.com>
2025-10-11 22:44:46 +0500

    add index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', padding: '50px' }}>
+      <h1>Hello Vibe Tech!</h1>
+      <p>Your Next.js app is running successfully.</p>
+    </div>
+  );
+}
