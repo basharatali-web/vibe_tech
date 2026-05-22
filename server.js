1695adbe6bd03facc5d14ff18a76a75247995e5a
basharatali-web <softwaredivision92@gmail.com>
2026-05-22 15:06:37 +0500

    fix server crash

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
 
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("🚀 API is LIVE and working");
+});
+
+app.get("/test", (req, res) => {
+  res.json({ status: "ok" });
+});
+
+// IMPORTANT: Render port fix
+const PORT = process.env.PORT || 10000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("Server running on port " + PORT);
+});
